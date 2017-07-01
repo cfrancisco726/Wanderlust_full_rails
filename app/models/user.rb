723a6d2bb024cdb22a40291018b3e03cca9830b9
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-07-01 12:19:42 -0400

    bundle updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :trips
-  has_many :hotels, through: :trips
-  has_many :flights, through: :trips
+  has_many :hotels, through: :trips, foreign_key: "hotel_id"
+  has_many :flights, through: :trips, foreign_key: "flight_id"
   has_secure_password
 end
