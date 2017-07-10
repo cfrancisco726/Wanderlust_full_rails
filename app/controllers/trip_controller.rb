6a8ad7d7fb5f52218867661865c062940a24ebe7
Daniel Velez <email": null>
2017-07-10 17:33:58 -0400

    styling forms

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -7,7 +7,13 @@ class TripController < ApplicationController
     @flight_data = ResponseFlightData.find(params[:id])
     @client = GooglePlaces::Client.new("AIzaSyAaWoHUrKrTUmI4tlDQi89bHPNWEYO34tQ")
     @attractions = @client.spots_by_query("Vacation attractions by #{convert_airportcode_to_destination(@flight_data[:destination])}")
-    binding.pry
+    @attractions.each do |atraction|
+      atraction.photos.each do |photo|
+        @photo ="https://maps.googleapis.com/maps/api/place/photo?maxwidth=#{photo.width}&photoreference=#{photo.photo_reference}&key=AIzaSyAaWoHUrKrTUmI4tlDQi89bHPNWEYO34tQ"
+      end
+    end
+    @attraction_photo = @attractions
+
   end
 
   def create
