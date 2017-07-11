f777e5e587a9947b7de466681f24c51f65899419
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-07-11 14:39:12 -0400

    helper methods complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -111,6 +111,104 @@ class ApplicationController < ActionController::Base
       "Los Angeles"
     elsif airport_code == "MIA"
       "Miami"
+    elsif airport_code == "FCO"
+      "Italy"
+    elsif airport_code == "SYD"
+      "Sydney"
+    elsif airport_code == "LHR"
+      "London"
+    elsif airport_code == "CDG"
+      "Paris"
+    elsif airport_code == "PRG"
+      "Prague"
+    elsif airport_code == "DXB"
+      "Dubai"  
+    elsif airport_code == "JTR"
+      "Santorini"
+    elsif airport_code == "HNL"
+      "Honolulu"
+    elsif airport_code == "DPS"
+      "Bali"    
+    elsif airport_code == "YVR"
+      "Vancouver"
+    elsif airport_code == "HKT"
+      "Phuket"
+    elsif airport_code == "EAS"
+      "San Sebastian"
+    elsif airport_code == "PPT"
+      "Tahiti"
+    elsif airport_code == "FAT"
+      "Yosemite"
+    elsif airport_code == "SJO"
+      "Costa Rica"
+    elsif airport_code == "SLC"
+      "Salt Lake City"
+    elsif airport_code == "JAC"
+      "Jackson Hole"                
+    end
+  end
+
+  def lat_long(convert_airportcode_to_destination(airport_code))
+      if "Denver"
+        lat = 39.8561
+        lng = -104.6737
+    elsif "Los Angeles"
+        lat = 33.9416
+        lng = -118.4085
+    elsif "Miami"
+        lat = 25.7959
+        lng = -80.2870
+    elsif "Italy"
+        lat = 41.7998868
+        lng = 12.246238400000038
+    elsif "Sydney"
+        lat = -33.8688
+        lng = 151.2093
+    elsif "London"
+        lat = 51.5074
+        lng = -0.1278
+    elsif "Paris"
+        lat = 48.8566
+        lng = 2.3522
+    elsif "Prague"
+        lat = 50.0755
+        lng = 14.4378
+    elsif "Dubai"  
+        lat = 25.2048
+        lng = 55.2708
+    elsif "Santorini"
+        lat = 36.3932
+        lng = 25.4615
+    elsif "Honolulu"
+        lat = 21.3069
+        lng = -157.8583
+    elsif "Bali" 
+        lat = -8.409518
+        lng = 115.188919 
+    elsif "Vancouver"
+        lat = 49.2827
+        lng = -123.1207
+    elsif  "Phuket" 
+        lat = 7.9519
+        lng = 98.3381
+    elsif "San Sebastian"
+        lat = 43.3183
+        lng = -1.9812
+    elsif "Tahiti"
+        lat = -17.6509
+        lng = -149.4260
+    elsif "Yosemite"
+        lat = 37.8651
+        lng = -119.5383
+    elsif "Costa Rica"
+        lat = 9.7489
+        lng = -83.7534
+    elsif "Salt Lake City"
+        lat = 40.7608
+        lng = -111.8910
+    elsif "Jackson Hole" 
+        lat = 43.4799
+        lng = -110.7624               
     end
   end
 
