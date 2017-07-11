0ff72fbe690849d88f3d354de5b034d42983f8bd
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-07-11 17:04:23 -0400

    rewrote helper methods with tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :api_call, :parse_api_response, :convert_airportcode_to_destination, :flights_date
+  helper_method :lat_long, :current_user, :logged_in?, :api_call, :parse_api_response, :convert_airportcode_to_destination, :flights_date
 
 
   def current_user
@@ -148,69 +148,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def lat_long(convert_airportcode_to_destination(airport_code))
-      if "Denver"
-        lat = 39.8561
-        lng = -104.6737
-    elsif "Los Angeles"
-        lat = 33.9416
-        lng = -118.4085
-    elsif "Miami"
-        lat = 25.7959
-        lng = -80.2870
-    elsif "Italy"
-        lat = 41.7998868
-        lng = 12.246238400000038
-    elsif "Sydney"
-        lat = -33.8688
-        lng = 151.2093
-    elsif "London"
-        lat = 51.5074
-        lng = -0.1278
-    elsif "Paris"
-        lat = 48.8566
-        lng = 2.3522
-    elsif "Prague"
-        lat = 50.0755
-        lng = 14.4378
-    elsif "Dubai"  
-        lat = 25.2048
-        lng = 55.2708
-    elsif "Santorini"
-        lat = 36.3932
-        lng = 25.4615
-    elsif "Honolulu"
-        lat = 21.3069
-        lng = -157.8583
-    elsif "Bali" 
-        lat = -8.409518
-        lng = 115.188919 
-    elsif "Vancouver"
-        lat = 49.2827
-        lng = -123.1207
-    elsif  "Phuket" 
-        lat = 7.9519
-        lng = 98.3381
-    elsif "San Sebastian"
-        lat = 43.3183
-        lng = -1.9812
-    elsif "Tahiti"
-        lat = -17.6509
-        lng = -149.4260
-    elsif "Yosemite"
-        lat = 37.8651
-        lng = -119.5383
-    elsif "Costa Rica"
-        lat = 9.7489
-        lng = -83.7534
-    elsif "Salt Lake City"
-        lat = 40.7608
-        lng = -111.8910
-    elsif "Jackson Hole" 
-        lat = 43.4799
-        lng = -110.7624               
-    end
-  end
+  
 
 
-end
