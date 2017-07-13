01e3a19d3d622ac12980d0bdbaf02c65f05d0111
Minkee Sung <minkeesung@gmail.com>
2017-07-12 22:29:55 -0400

    made changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
 
   def current_user
-    @user ||= User.find_by(id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
   end
 
   def logged_in?
