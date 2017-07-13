74fbb377ef81a557fea817d85b39a30df1e65b9b
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-07-13 11:43:05 -0400

    moved CSS to trips.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   // debugger
   $('body').on("click", '.save-flight-button', function(e){
     e.preventDefault();
-    debugger
     $.ajax({
       url: $(e.target).attr('href'),
       method: 'get'
