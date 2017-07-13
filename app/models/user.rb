06e8f871fc6b7d8ad48f75237002ddab5241a549
cfrancisco726 <cfrancisco726@gmail.com>
2017-07-12 20:59:28 -0400

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   has_many :trips
   has_many :hotels, through: :trips, foreign_key: "hotel_id"
   has_many :flights, through: :trips, source: :flights
-  has_many :save_user_trips 
+  has_many :save_user_trips
+
+  validates :email, :password_digest, :name, presence: true
+  validates :password_digesr, length: { in: 6..20 }
 
   has_secure_password
 end
