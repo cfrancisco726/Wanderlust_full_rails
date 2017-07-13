c7326f59b6656dfb6813633bd8675c17af902bf5
cfrancisco726 <cfrancisco726@gmail.com>
2017-07-13 10:43:47 -0400

    loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 
-  // debugger
   $('body').on("click", '.save-flight-button', function(e){
     e.preventDefault();
-    debugger
     $.ajax({
       url: $(e.target).attr('href'),
       method: 'get'
