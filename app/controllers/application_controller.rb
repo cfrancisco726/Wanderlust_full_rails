86e1e00c932e0642cc58e9655e0466c2f18dc3c8
Minkee Sung <minkeesung@gmail.com>
2017-07-14 11:07:35 -0400

    added more locations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
             }
           ],
           "maxPrice": "USD#{budget}",
-          "solutions": "10"
+          "solutions": "1"
         }
       }
   end
